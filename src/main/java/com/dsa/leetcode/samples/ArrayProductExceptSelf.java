317f14c36d62dc1c7b0ec3ffdff20422dfc7de75
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-20 12:57:01 +0530

    started the day with array problem

---
+++ src/main/java/com/dsa/leetcode/samples/ArrayProductExceptSelf.java
@@ -0,0 +1,24 @@
+package com.dsa.leetcode.samples;
+
+public class ArrayProductExceptSelf {
+
+	public static void main(String[] args) {
+		int[] nums = new int[] { 1, 2, 4, 6 };
+		System.out.println(productExceptSelf(nums));
+	}
+
+	private static int[] productExceptSelf(int[] nums) {
+		int[] resultArr = new int[nums.length];
+		for (int iIndex = 0; iIndex < nums.length; iIndex++) {
+			int product = 1;
+			for (int jIndex = 0; jIndex < nums.length; jIndex++) {
+				if(jIndex != iIndex) {
+					product = product * nums[jIndex];
+				}
+			}
+			resultArr[iIndex] = product;
+		}
+		return resultArr;
+	}
+
+}
