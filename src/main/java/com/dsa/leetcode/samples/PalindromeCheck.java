c237a4a6395df715ad2afedc23fdb6bb583bda0f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-20 17:32:30 +0530

    finished the palindrome check code

--- src/main/java/com/dsa/leetcode/samples/PalindromeCheck.java
+++ src/main/java/com/dsa/leetcode/samples/PalindromeCheck.java
@@ -3,9 +3,15 @@ package com.dsa.leetcode.samples;
 public class PalindromeCheck {
 
 	public static void main(String[] args) {
-		StringBuilder sb1 = new StringBuilder("Was it a car or a cat I saw?");
+		String str = "Was it a car or a cat I saw?";
+		System.out.println(isPalindrome(str));
+	}
+
+	private static boolean isPalindrome(String s) {
+		StringBuilder sb1 = new StringBuilder(s);
 		String cleanStr = sb1.toString().replaceAll("[^\\w-]", "").toLowerCase().replaceAll("\\s+", " ");
 		StringBuilder sb2 = new StringBuilder(cleanStr).reverse();
-		System.out.println(sb2);
+		return cleanStr.equals(sb2.toString());
+
 	}
 }
