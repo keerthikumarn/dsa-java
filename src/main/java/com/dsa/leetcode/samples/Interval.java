06b2118861c21e33c03828fb90d3f48f8aef7006
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-25 10:17:49 +0530

    solved the meeting slot problem

---
+++ src/main/java/com/dsa/leetcode/samples/Interval.java
@@ -0,0 +1,10 @@
+package com.dsa.leetcode.samples;
+
+public class Interval {
+	public int start, end;
+
+	public Interval(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+}
---
+++ src/main/java/com/dsa/leetcode/samples/MeetingProblem.java
@@ -0,0 +1,26 @@
+package com.dsa.leetcode.samples;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MeetingProblem {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static boolean canAttendMeetings(List<Interval> intervals) {
+		// sort the intervals
+		Collections.sort(intervals, Comparator.comparingInt(interval -> interval.start));
+		for (int idx = 1; idx < intervals.size(); idx++) {
+			Interval interval1 = intervals.get(idx - 1);
+			Interval interval2 = intervals.get(idx);
+			if (interval1.start < interval2.start) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
