a82b414889ada7ea457b7f2fc453d848adb3a02e
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-25 10:25:30 +0530

    solved the crypto buy n sell problem

---
+++ src/main/java/com/dsa/leetcode/samples/BuyAndSellCrypto.java
@@ -0,0 +1,24 @@
+package com.dsa.leetcode.samples;
+
+public class BuyAndSellCrypto {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static int maxProfit(int[] stockPrices) {
+		int right = 1;
+		int left = 0;
+		int maxProfit = 0;
+		while (right < stockPrices.length) {
+			if (stockPrices[left] < stockPrices[right]) {
+				maxProfit = Math.max(maxProfit, stockPrices[right] - stockPrices[left]);
+			} else {
+				left = right;
+			}
+			right++;
+		}
+		return maxProfit;
+	}
+
+}
