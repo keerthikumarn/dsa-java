85bbf8114e0abbd6cdaf2b99acc2504d8c87b6c3
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-26 11:20:17 +0530

    solved the happy number check

---
+++ src/main/java/com/dsa/leetcode/samples/HappyNumberCheck.java
@@ -0,0 +1,34 @@
+package com.dsa.leetcode.samples;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumberCheck {
+
+	public static void main(String[] args) {
+		System.out.println(isHappy(100));
+		System.out.println(isHappy(2));
+	}
+
+	private static boolean isHappy(int num) {
+		Set<Integer> numSet = new HashSet<>();
+		while (num != 1) {
+			if (!numSet.add(num)) {
+				return false;
+			}
+			num = nextNumber(num);
+		}
+		return true;
+	}
+
+	private static int nextNumber(int num) {
+		int totalSum = 0;
+		while (num > 0) {
+			int digit = num % 10;
+			totalSum = totalSum + (digit * digit);
+			num = num / 10;
+		}
+		return totalSum;
+	}
+
+}
