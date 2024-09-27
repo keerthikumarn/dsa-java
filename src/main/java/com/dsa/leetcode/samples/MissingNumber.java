0a3469de5525b590d59c2147d340b274a4882702
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-27 17:34:09 +0530

    solved the missing number problem

---
+++ src/main/java/com/dsa/leetcode/samples/MissingNumber.java
@@ -0,0 +1,18 @@
+package com.dsa.leetcode.samples;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		System.out.println(missingNumber(new int[] { 1, 2, 3 }));
+		System.out.println(missingNumber(new int[] { 0, 2 }));
+	}
+
+	private static int missingNumber(int[] nums) {
+		int missingNumber = nums.length;
+		for (int idx = 0; idx < nums.length; idx++) {
+			missingNumber = missingNumber + idx - nums[idx];
+		}
+		return missingNumber;
+	}
+
+}
