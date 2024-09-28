02b34ed06bbc145ab0070641328d1ccb85dcc7a2
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-28 22:50:49 +0530

    just one tree traversal problem

---
+++ src/main/java/com/dsa/leetcode/samples/BTLevelOrderTraversal.java
@@ -0,0 +1,39 @@
+package com.dsa.leetcode.samples;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import com.dsa.leetcode.samples.TreeNode;
+
+public class BTLevelOrderTraversal {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<>();
+		Deque<TreeNode> dq = new ArrayDeque<>();
+		if (root != null) {
+			dq.add(root);
+		}
+
+		while (!dq.isEmpty()) {
+			List<Integer> list = new ArrayList<>();
+			for (int i = 0, len = dq.size(); i < len; i++) {
+                TreeNode node = dq.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    dq.add(node.left);
+                }
+                if (node.right != null) {
+                    dq.add(node.right);
+                }
+            }
+            result.add(list);
+		}
+		return result;
+	}
+
+}
