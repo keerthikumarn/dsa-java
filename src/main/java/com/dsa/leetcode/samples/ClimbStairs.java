2007f46aea65ac42ad403ccf9c333a0d8a337ed3
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-09-29 16:16:25 +0530

    implemented climbing the stairs problem

---
+++ src/main/java/com/dsa/leetcode/samples/ClimbStairs.java
@@ -0,0 +1,24 @@
+package com.dsa.leetcode.samples;
+
+public class ClimbStairs {
+
+	public static void main(String[] args) {
+		System.out.println(climbStairs(2));
+		System.out.println(climbStairs(3));
+		System.out.println(climbStairs(5));
+	}
+
+	private static int climbStairs(int n) {
+		if (n == 0 || n == 1) {
+			return 1;
+		}
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int idx = 2; idx <= n; idx++) {
+			dp[idx] = dp[idx - 1] + dp[idx - 2];
+		}
+		return dp[n];
+	}
+
+}
