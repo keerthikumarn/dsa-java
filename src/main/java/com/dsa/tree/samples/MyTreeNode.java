f8370deeb1de6a523df138d31177a46df1165153
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-05 09:30:10 +0530

    started with the tree data structure

---
+++ src/main/java/com/dsa/tree/samples/MyTreeNode.java
@@ -0,0 +1,28 @@
+package com.dsa.tree.samples;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyTreeNode {
+
+	private List<MyTreeNode> childNodes;
+	private String value;
+
+	public MyTreeNode(String value) {
+		this.value = value;
+		this.childNodes = new LinkedList<>();
+	}
+
+	public void addChildNode(MyTreeNode childNode) {
+		childNodes.add(childNode);
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public List<MyTreeNode> getChildNodes() {
+		return childNodes;
+	}
+
+}
