072cfdc96faa70eccac8281dc77c95f4992cd678
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-08 10:39:40 +0530

    solved the last stone weight problem

---
+++ src/main/java/com/dsa/leetcode/samples/LastStoneWeight.java
@@ -0,0 +1,27 @@
+package com.dsa.leetcode.samples;
+
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+
+	public static void main(String[] args) {
+		System.out.println(lastStoneWeight(new int[] { 2, 3, 6, 2, 4 }));
+	}
+
+	private static int lastStoneWeight(int[] stones) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
+		for (int stone : stones) {
+			queue.offer(-stone);
+		}
+		while (queue.size() > 1) {
+			int firstNum = queue.poll();
+			int secondNum = queue.poll();
+			if (secondNum > firstNum) {
+				queue.offer(firstNum - secondNum);
+			}
+		}
+		queue.offer(0);
+		return Math.abs(queue.peek());
+	}
+
+}
