1b62fd94e6ef488cd4bb775a2744487fab812ac8
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-09 11:22:36 +0530

    started with a tree problem

---
+++ src/main/java/com/dsa/leetcode/samples/IsSameTree.java
@@ -0,0 +1,20 @@
+package com.dsa.leetcode.samples;
+
+public class IsSameTree {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if (p != null && q != null && p.val == q.val) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		} else {
+			return false;
+		}
+	}
+
+}
