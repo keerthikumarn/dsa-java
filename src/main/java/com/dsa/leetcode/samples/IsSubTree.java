3575c12d145037c2b8b9c069e0e134e77695d72d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-14 11:30:54 +0530

    started with a small tree problem

---
+++ src/main/java/com/dsa/leetcode/samples/IsSubTree.java
@@ -0,0 +1,33 @@
+package com.dsa.leetcode.samples;
+
+public class IsSubTree {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private static boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root != null && subRoot != null && root.val == subRoot.val) {
+            return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+        }
+        return false;
+    }}
+
+}
