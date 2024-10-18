293f6ceb2f58c56f84c761d71265606fcc2690a9
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-18 11:00:08 +0530

    solved viral ad problem from hackerank

---
+++ src/main/java/com/dsa/leetcode/samples/ViralAdvertising.java
@@ -0,0 +1,22 @@
+package com.dsa.leetcode.samples;
+
+public class ViralAdvertising {
+
+	public static void main(String[] args) {
+		int days = 5; // Number of days to report
+		int result = viralAdvertising(days);
+		System.out.println("Cumulative Likes by Day " + days + ": " + result);
+	}
+
+	private static int viralAdvertising(int n) {
+		int noOfAdsShared = 5;
+		int likes = 0;
+		for (int day = 1; day <= n; day++) {
+			int liked = noOfAdsShared / 2;
+			likes += liked;
+			noOfAdsShared = liked * 3;
+		}
+		return likes;
+	}
+
+}
