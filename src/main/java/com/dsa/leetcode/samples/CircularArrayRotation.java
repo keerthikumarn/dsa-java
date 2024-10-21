d41d2443e372f6cbd3cc4e33498f028bd5d89be2
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-21 22:38:38 +0530

    implemented circular array rotation

---
+++ src/main/java/com/dsa/leetcode/samples/CircularArrayRotation.java
@@ -0,0 +1,23 @@
+package com.dsa.leetcode.samples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CircularArrayRotation {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+		int arrSize = a.size();
+		k = k % arrSize;
+		List<Integer> result = new ArrayList<>();
+		for (int query : queries) {
+			int newIndex = (query - k + arrSize) % arrSize;
+			result.add(a.get(newIndex));
+		}
+		return result;
+	}
+
+}
