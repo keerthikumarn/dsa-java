c83a1f208e17498d8a43a02ce42ec2f9dca3c85a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-10-22 15:41:21 +0530

    Two integer solution with HashMap approach

--- src/main/java/com/dsa/leetcode/samples/TwoIntegerSum.java
+++ src/main/java/com/dsa/leetcode/samples/TwoIntegerSum.java
@@ -1,5 +1,8 @@
 package com.dsa.leetcode.samples;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoIntegerSum {
 
 	public static void main(String[] args) {
@@ -14,6 +17,19 @@ public class TwoIntegerSum {
 					return new int[] { iIdx + 1, jIdx + 1 };
 				}
 			}
+		}
+		return new int[0];
+	}
+
+	// HashMap approach
+	private static int[] twoSumHashMapApproach(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int iIdx = 0; iIdx < nums.length; iIdx++) {
+			int num = target - nums[iIdx];
+			if (map.containsKey(num)) {
+				return new int[] { map.get(num), iIdx + 1 };
+			}
+			map.put(nums[iIdx], iIdx + 1);
 		}
 		return new int[0];
 	}
