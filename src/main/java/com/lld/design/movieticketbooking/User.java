af534cea20c000d2008cf3cb66492d4d77207a50
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-01 16:13:39 +0530

    Added the user entity

--- src/main/java/com/lld/design/movieticketbooking/User.java
+++ src/main/java/com/lld/design/movieticketbooking/User.java
@@ -1,5 +1,18 @@
 package com.lld.design.movieticketbooking;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class User {
+
+	private String id;
+	private String name;
+	private String email;
 
 }
