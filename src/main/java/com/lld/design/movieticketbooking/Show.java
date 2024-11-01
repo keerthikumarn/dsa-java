915da2b197bc96de610b00e07b50b39230a68c17
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-01 16:38:18 +0530

    added the entity of Show class

--- src/main/java/com/lld/design/movieticketbooking/Show.java
+++ src/main/java/com/lld/design/movieticketbooking/Show.java
@@ -3,11 +3,20 @@ package com.lld.design.movieticketbooking;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Show {
 	
 	private String id;
     private Movie movie;
-    private Theater theater;
+    private Theatre theatre;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private Map<String, Seat> seats;
