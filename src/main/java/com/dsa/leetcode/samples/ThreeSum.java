d79c59d76fc296a654d8bab9027a5c8ae6ba5f5d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-03 11:24:34 +0530

    implemented the 3 Sum problem

--- src/main/java/com/dsa/leetcode/samples/ThreeSum.java
+++ src/main/java/com/dsa/leetcode/samples/ThreeSum.java
@@ -2,8 +2,10 @@ package com.dsa.leetcode.samples;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ThreeSum {
@@ -30,6 +32,36 @@ public class ThreeSum {
 			}
 		}
 		return new ArrayList<>(result);
+	}
+
+	/* HashMap approach */
+	private static List<List<Integer>> threeSumHashMapApproach(int[] nums) {
+		Arrays.sort(nums);
+		Map<Integer, Integer> countMap = new HashMap<>();
+		for (int num : nums) {
+			countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+		}
+		List<List<Integer>> result = new ArrayList<>();
+		for (int iIdx = 0; iIdx < nums.length; iIdx++) {
+			countMap.put(nums[iIdx], countMap.get(nums[iIdx] - 1));
+			if (iIdx > 0 && nums[iIdx] == nums[iIdx - 1]) {
+				continue;
+			}
+			for (int jIdx = iIdx + 1; jIdx < nums.length; jIdx++) {
+				countMap.put(nums[jIdx], countMap.get(nums[jIdx]) - 1);
+				if (jIdx > iIdx + 1 && nums[jIdx] == nums[jIdx - 1]) {
+					continue;
+				}
+				int target = -(nums[iIdx] + nums[jIdx]);
+				if (countMap.getOrDefault(target, 0) > 0) {
+					result.add(Arrays.asList(nums[iIdx], nums[jIdx], target));
+				}
+			}
+			for (int kIdx = iIdx + 1; kIdx < nums.length; kIdx++) {
+				countMap.put(nums[kIdx], countMap.get(nums[kIdx]) + 1);
+			}
+		}
+		return result;
 	}
 
 }
