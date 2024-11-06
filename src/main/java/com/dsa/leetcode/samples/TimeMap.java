77fb58b8ac319feb8d7fccb49cb848e0dd2a5710
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-06 12:07:09 +0530

    solved TimeMap problem

---
+++ src/main/java/com/dsa/leetcode/samples/TimeMap.java
@@ -0,0 +1,28 @@
+package com.dsa.leetcode.samples;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TimeMap {
+
+	private Map<String, TreeMap<Integer, String>> timeMap;
+
+	public TimeMap() {
+		timeMap = new HashMap<>();
+	}
+
+	public void set(String key, String value, int timestamp) {
+		timeMap.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+	}
+
+	public String get(String key, int timestamp) {
+		if (!timeMap.containsKey(key)) {
+			return "";
+		}
+		TreeMap<Integer, String> timestamps = timeMap.get(key);
+		Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
+		return entry == null ? "" : entry.getValue();
+	}
+
+}
