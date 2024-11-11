59d66db0af2b89c6fa703a1c9f879864f076afcd
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-11 10:31:32 +0530

    updated the solution with binary search approach

--- src/main/java/com/dsa/leetcode/samples/Search2DMatrix.java
+++ src/main/java/com/dsa/leetcode/samples/Search2DMatrix.java
@@ -4,6 +4,7 @@ public class Search2DMatrix {
 
 	public static void main(String[] args) {
 		System.out.println(searchMatrix(new int[][] { { 1, 2, 4, 8 }, { 10, 11, 12, 13 }, { 14, 20, 30, 40 } }, 20));
+		System.out.println(searchMatrixBSApproach(new int[][] { { 1, 2, 4, 8 }, { 10, 11, 12, 13 }, { 14, 20, 30, 40 } }, 20));
 	}
 
 	/* Brute force approach */
@@ -14,6 +15,27 @@ public class Search2DMatrix {
 					System.out.println("Target element: " + target + " is found");
 					return true;
 				}
+			}
+		}
+		return false;
+	}
+
+	/* Binary Search approach */
+	private static boolean searchMatrixBSApproach(int[][] matrix, int target) {
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		int left = 0;
+		int right = rows * cols - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			int row = mid / cols;
+			int col = mid % cols;
+			if (target > matrix[row][col]) {
+				left = mid + 1;
+			} else if (target < matrix[row][col]) {
+				right = mid - 1;
+			} else {
+				return true;
 			}
 		}
 		return false;
