94d8f619ae5db3fc4a51effa16e0087061821093
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-11 23:24:02 +0530

    improved the solution with 2 ptr approach

--- src/main/java/com/dsa/leetcode/samples/MaxWaterContainer.java
+++ src/main/java/com/dsa/leetcode/samples/MaxWaterContainer.java
@@ -4,6 +4,7 @@ public class MaxWaterContainer {
 
 	public static void main(String[] args) {
 		System.out.println(maxArea(new int[] { 1, 7, 2, 5, 4, 7, 3, 6 }));
+		System.out.println(maxAreaTwoPointerApproach(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
 	}
 
 	/* Brute Force Approach */
@@ -14,6 +15,27 @@ public class MaxWaterContainer {
 				maxArea = Math.max(maxArea, Math.min(heights[iIdx], heights[jIdx]) * (jIdx - iIdx));
 			}
 		}
+		return maxArea;
+	}
+
+	/* 2 Pointer Approach */
+	private static int maxAreaTwoPointerApproach(int[] heights) {
+		int leftPtr = 0;
+		int rightPtr = heights.length - 1;
+		int maxArea = 0;
+		while (leftPtr < rightPtr) {
+			System.out.println("heights[leftPtr]: "+heights[leftPtr]+" and heights[rightPtr]: "+ heights[rightPtr]);
+			int currArea = (rightPtr - leftPtr) * Math.min(heights[leftPtr], heights[rightPtr]);
+			System.out.println("currArea: "+currArea);
+			maxArea = Math.max(maxArea, currArea);
+			System.out.println("maxArea: "+maxArea);
+			if (heights[leftPtr] < heights[rightPtr]) {
+				leftPtr = leftPtr + 1;
+			} else {
+				rightPtr = rightPtr - 1;
+			}
+		}
+		System.out.println("Final Max Area: "+maxArea);
 		return maxArea;
 	}
 
