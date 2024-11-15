d6e387584ca18b050384d41063861c2ee0ea87be
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-15 22:56:11 +0530

    solved rotate image problem

---
+++ src/main/java/com/dsa/leetcode/samples/RotateImage.java
@@ -0,0 +1,34 @@
+package com.dsa.leetcode.samples;
+
+public class RotateImage {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static void rotate(int[][] matrix) {
+		int len = matrix.length;
+		for (int iIdx = 0; iIdx < len; iIdx++) {
+			for (int jIdx = iIdx + 1; jIdx < len; jIdx++) {
+				int temp = matrix[iIdx][jIdx];
+				matrix[iIdx][jIdx] = matrix[jIdx][iIdx];
+				matrix[jIdx][iIdx] = temp;
+			}
+		}
+		// reverse the array
+		for (int idx = 0; idx < len; idx++) {
+			reverseArray(matrix[idx]);
+		}
+	}
+
+	private static void reverseArray(int[] matrix) {
+		int n = matrix.length;
+		for (int i = 0; i < n / 2; i++) {
+			int temp = matrix[i];
+            matrix[i] = matrix[n - 1 - i];
+            matrix[n - 1 - i] = temp;
+        }
+	}
+
+}
+	
