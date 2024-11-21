ebf6e47c4a5229aef06b4240ffd307b7af049c57
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-21 16:21:17 +0530

    included the soln with binary search approach

--- src/main/java/com/dsa/leetcode/samples/MinEatingSpeed.java
+++ src/main/java/com/dsa/leetcode/samples/MinEatingSpeed.java
@@ -1,9 +1,12 @@
 package com.dsa.leetcode.samples;
 
+import java.util.Arrays;
+
 public class MinEatingSpeed {
 
 	public static void main(String[] args) {
 		System.out.println(minEatingSpeed(new int[] { 1, 4, 3, 2 }, 9));
+		System.out.println(minEatingSpeedBS(new int[] { 1, 4, 3, 2 }, 9));
 	}
 
 	/* Brute force approach */
@@ -19,6 +22,27 @@ public class MinEatingSpeed {
 			}
 			eatingSpeed++;
 		}
+	}
+
+	/* Binary search approach */
+	private static int minEatingSpeedBS(int[] piles, int h) {
+		int left = 1;
+		int right = Arrays.stream(piles).max().getAsInt();
+		int minEatingSpeed = right;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			long totalTimeTaken = 0;
+			for (int pile : piles) {
+				totalTimeTaken = totalTimeTaken + (int) Math.ceil((double) pile / mid);
+			}
+			if (totalTimeTaken <= h) {
+				minEatingSpeed = mid;
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return minEatingSpeed;
 	}
 
 }
