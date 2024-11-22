a5b6f5d49860b1178ed0e06ad74ead7609636c50
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-22 23:23:48 +0530

    solved Kth smallest element in array problem

---
+++ src/main/java/com/dsa/leetcode/samples/KthSmallestElementInArray.java
@@ -0,0 +1,29 @@
+package com.dsa.leetcode.samples;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInArray {
+
+	public static void main(String[] args) {
+		System.out.println(kthSmallest(new Integer[] { 12, 3, 5, 7, 19 }, 2));
+		System.out.println(kthSmallestWithPQ(new Integer[] { 12, 3, 5, 7, 19 }, 2));
+	}
+
+	private static int kthSmallest(Integer[] arr, int K) {
+		Arrays.sort(arr);
+		return arr[K - 1];
+	}
+
+	private static int kthSmallestWithPQ(Integer[] arr, int K) {
+		PriorityQueue<Integer> pQueue = new PriorityQueue<>((a, b) -> b - a);
+		for (int idx = 0; idx < arr.length; idx++) {
+			pQueue.offer(arr[idx]);
+			if (pQueue.size() > K) {
+				pQueue.poll();
+			}
+		}
+		return pQueue.peek();
+	}
+
+}
