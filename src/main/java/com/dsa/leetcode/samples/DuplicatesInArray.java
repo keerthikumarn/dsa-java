5e8c5579a250ee1e3c78f3f7509dd5d85a439010
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-23 00:02:13 +0530

    solved duplicates in array problem

---
+++ src/main/java/com/dsa/leetcode/samples/DuplicatesInArray.java
@@ -0,0 +1,32 @@
+package com.dsa.leetcode.samples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DuplicatesInArray {
+
+	public static void main(String[] args) {
+		List<Integer> resList = findDuplicates(Arrays.asList(12, 11, 40, 12, 5, 6, 5, 12, 11));
+		for (int num : resList) {
+			System.out.println(num);
+		}
+	}
+
+	private static List<Integer> findDuplicates(List<Integer> arr) {
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		for (int num : arr) {
+			freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+		}
+		List<Integer> duplicatesList = new ArrayList<>();
+		for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+			if (entry.getValue() > 1) {
+				duplicatesList.add(entry.getKey());
+			}
+		}
+		return duplicatesList;
+	}
+
+}
