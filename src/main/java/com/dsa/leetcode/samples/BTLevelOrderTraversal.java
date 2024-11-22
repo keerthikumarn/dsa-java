0adf15263d8921de143e8678efb9e2eea9df5502
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-22 23:15:53 +0530

    minor cleanup

--- src/main/java/com/dsa/leetcode/samples/BTLevelOrderTraversal.java
+++ src/main/java/com/dsa/leetcode/samples/BTLevelOrderTraversal.java
@@ -4,7 +4,6 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
-import com.dsa.leetcode.samples.TreeNode;
 
 public class BTLevelOrderTraversal {
 
