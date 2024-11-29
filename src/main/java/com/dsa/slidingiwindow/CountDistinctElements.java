f7f622769f22c4dabce86c3d09936e6dcb818ff3
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-11-29 13:36:41 +0530

    solved using SW approach

--- src/main/java/com/dsa/slidingiwindow/CountDistinctElements.java
+++ src/main/java/com/dsa/slidingiwindow/CountDistinctElements.java
@@ -1,9 +1,13 @@
 package com.dsa.slidingiwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDistinctElements {
 
 	public static void main(String[] args) {
 		System.out.println(countWindowDistinct(new int[] { 1, 2, 1, 3, 4, 2, 3 }, 4));
+		System.out.println(countWindowDistinctSW(new int[] { 1, 2, 1, 3, 4, 2, 3 }, 4));
 	}
 
 	/* Brute force approach */
@@ -21,5 +25,26 @@ public class CountDistinctElements {
 			}
 		}
 		return distinctCount;
+	}
+
+	/* Sliding window approach */
+	private static int countWindowDistinctSW(int win[], int K) {
+		Map<Integer, Integer> map = new HashMap<>();
+		// loop thru the 1st window and get the count
+		for (int idx = 0; idx < K; idx++) {
+			map.put(win[idx], map.getOrDefault(win[idx], 0) + 1);
+		}
+		// loop thru the remaining array
+		for (int idx = K; idx < win.length; idx++) {
+			// remove the 1st element from the previous window
+			if (map.get(win[idx - K]) == 1) {
+				map.remove(win[idx - K]);
+			} else {
+				// reduce the count of the remaining element
+				map.put(win[idx - K], map.get(win[idx - K]) - 1);
+			}
+			map.put(win[idx], map.getOrDefault(win[idx], 0) + 1);
+		}
+		return map.size();
 	}
 }
