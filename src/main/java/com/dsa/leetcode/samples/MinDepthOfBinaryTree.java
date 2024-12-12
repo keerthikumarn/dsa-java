1b25d07e1e0806e06e1b7a26f3f2d831b98c234f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-12-12 11:12:16 +0530

    min depth of BT

---
+++ src/main/java/com/dsa/leetcode/samples/MinDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package com.dsa.leetcode.samples;
+
+public class MinDepthOfBinaryTree {
+
+	public static void main(String[] args) {
+
+	}
+
+	private static int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.left == null) {
+			return 1 + minDepth(root.right);
+		}
+		if (root.right == null) {
+			return 1 + minDepth(root.left);
+		}
+		return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+	}
+
+}
