80114610c8e8cc8985f3ffeea1e2dfc15809ec55
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-12-19 11:34:04 +0530

    added the order item module

---
+++ src/main/java/com/ddd/demo/OrderItem.java
@@ -0,0 +1,19 @@
+package com.ddd.demo;
+
+public class OrderItem {
+	private final String productId;
+	private final int quantity;
+
+	public OrderItem(String productId, int quantity) {
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+}
