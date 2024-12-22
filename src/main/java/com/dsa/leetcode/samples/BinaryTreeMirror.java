f31b291397ce42d060edadfd0e588f03fe192e6e
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-12-22 19:48:11 +0530

    solved mirror of BT

---
+++ src/main/java/com/dsa/leetcode/samples/BinaryTreeMirror.java
@@ -0,0 +1,44 @@
+package com.dsa.leetcode.samples;
+
+public class BinaryTreeMirror {
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		root.right.left = new TreeNode(6);
+		root.right.right = new TreeNode(7);
+		convertToMirrorOfTree(root);
+		printTransformedTree(root);
+	}
+
+	private static void printTransformedTree(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.val + " ");
+		printTransformedTree(root.left);
+		printTransformedTree(root.right);
+
+	}
+
+	private static void convertToMirrorOfTree(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		convertToMirrorOfTree(root.left);
+		convertToMirrorOfTree(root.right);
+		swapTree(root);
+	}
+
+	private static void swapTree(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		TreeNode tempNode = root.left;
+		root.left = root.right;
+		root.right = tempNode;
+	}
+}
