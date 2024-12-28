b71e5d3ead86f954c9df9d205c30eaf538a09aa0
Keerthi Kumar <n.keerthikumar85@gmail.com>
2024-12-28 17:33:44 +0530

    solved BST validation

---
+++ src/main/java/com/dsa/bst/demo/CheckIfTreeIsBST.java
@@ -0,0 +1,36 @@
+package com.dsa.bst.demo;
+
+public class CheckIfTreeIsBST {
+
+	private Node root;
+
+	public CheckIfTreeIsBST() {
+		this.root = null;
+	}
+
+	public boolean isBSTOrNot() {
+		return isBSTOrNot(this.root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	private boolean isBSTOrNot(Node root, int minValue, int maxValue) {
+		if (root == null) {
+			return true;
+		}
+		if (root.key >= minValue && root.key <= maxValue && isBSTOrNot(root.left, minValue, root.key)
+				&& isBSTOrNot(root.right, root.key, maxValue)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		CheckIfTreeIsBST bt = new CheckIfTreeIsBST();
+		bt.root = new Node(100);
+		bt.root.left = new Node(90);
+		bt.root.right = new Node(110);
+		bt.root.left.left = new Node(80);
+		bt.root.left.right = new Node(95);
+		System.out.println(bt.isBSTOrNot());
+	}
+
+}
