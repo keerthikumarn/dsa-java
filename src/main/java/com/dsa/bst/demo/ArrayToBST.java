5ca88e62fa436d052287bf218edbfd87e2429e29
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-12 21:00:34 +0530

    solved array to BST

---
+++ src/main/java/com/dsa/bst/demo/ArrayToBST.java
@@ -0,0 +1,35 @@
+package com.dsa.bst.demo;
+
+public class ArrayToBST {
+
+	public static void main(String[] args) {
+		ArrayToBST obj = new ArrayToBST();
+		int[] arr = { 1, 2, 3, 4 };
+		Node root = obj.sortedArrayToBST(arr);
+		obj.preOrder(root);
+	}
+
+	public Node sortedArrayToBSTRecursion(int[] arr, int start, int end) {
+		if (start > end) {
+			return null;
+		}
+		int mid = start + (end - start) / 2;
+		Node root = new Node(arr[mid]);
+		root.left = sortedArrayToBSTRecursion(arr, start, mid - 1);
+		root.right = sortedArrayToBSTRecursion(arr, mid + 1, end);
+		return root;
+	}
+
+	private Node sortedArrayToBST(int[] arr) {
+		return sortedArrayToBSTRecursion(arr, 0, arr.length - 1);
+	}
+
+	private void preOrder(Node root) {
+		if (root == null)
+			return;
+		System.out.print(root.key + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+
+}
