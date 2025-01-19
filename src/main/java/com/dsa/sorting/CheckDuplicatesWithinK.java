8493ee3df20d15a360e0d0f03e0502deb073c959
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-19 07:44:24 +0530

    minor changes

--- src/main/java/com/dsa/sorting/CheckDuplicatesWithinK.java
+++ src/main/java/com/dsa/sorting/CheckDuplicatesWithinK.java
@@ -2,12 +2,7 @@ package com.dsa.sorting;
 
 public class CheckDuplicatesWithinK {
 
-	public static void main(String[] args) {
-		int[] arr = { 10, 5, 3, 4, 3, 5, 6 };
-		System.out.println(checkDuplicatesWithinK(arr, 3) ? "Yes" : "No");
-	}
-
-	private static boolean checkDuplicatesWithinK(int[] arr, int k) {
+	public boolean checkDuplicatesWithinK(int[] arr, int k) {
 		int len = arr.length;
 		for (int idx = 0; idx < len; idx++) {
 			for (int c = 1; c <= k && (idx + c) < len; c++) {
