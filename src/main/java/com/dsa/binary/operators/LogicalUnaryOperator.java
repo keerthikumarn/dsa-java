8969f76c2985ee1b45809df0c7f9dcd54ac8d2a8
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-22 07:57:25 +0530

    learnt logical unary operator

---
+++ src/main/java/com/dsa/binary/operators/LogicalUnaryOperator.java
@@ -0,0 +1,11 @@
+package com.dsa.binary.operators;
+
+public class LogicalUnaryOperator {
+
+	public static void main(String[] args) {
+		boolean bool1 = true;
+		boolean bool2 = !bool1;
+		System.out.println("!bool1 = " + bool2);
+	}
+
+}
