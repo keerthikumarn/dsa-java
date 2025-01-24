bb56f8a7af4e6c99da2f9f539c2e0965ff9344ba
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-24 16:45:09 +0530

    removed the main method

--- src/main/java/com/dsa/twopointers/StringReverse.java
+++ src/main/java/com/dsa/twopointers/StringReverse.java
@@ -2,12 +2,8 @@ package com.dsa.twopointers;
 
 public class StringReverse {
 
-	public static void main(String[] args) {
-		System.out.println(strReverse("keerthi"));
-	}
-
 	/* 2 pointer approach */
-	private static String strReverse(String string) {
+	public String strReverse(String string) {
 		char[] chArr = string.toCharArray();
 		int leftPtr = 0;
 		int rightPtr = chArr.length - 1;
