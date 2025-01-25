796b2c0edda07ad211812443c4c67287b3b0c640
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-01-25 21:00:41 +0530

    started with the graph tutorials

---
+++ src/main/java/com/dsa/graph/demo/Edge.java
@@ -0,0 +1,15 @@
+package com.dsa.graph.demo;
+
+public class Edge {
+	int source;
+	int destination;
+	int weight;
+
+	public Edge(int source, int destination, int weight) {
+		super();
+		this.source = source;
+		this.destination = destination;
+		this.weight = weight;
+	}
+
+}
