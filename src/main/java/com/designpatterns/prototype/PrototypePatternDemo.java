020e181db0920338e6ed9aa36731fa048da5cc60
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-02-05 23:25:04 +0530

    included the client class

--- src/main/java/com/designpatterns/prototype/PrototypePatternDemo.java
+++ src/main/java/com/designpatterns/prototype/PrototypePatternDemo.java
@@ -1,9 +1,23 @@
 package com.designpatterns.prototype;
 
+import java.util.List;
+
 public class PrototypePatternDemo {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Vehicle vehicleA = new Vehicle();
+	    vehicleA.insertData();
+	    
+	    Vehicle vehicleB = (Vehicle) vehicleA.clone();
+	    List<String> list = vehicleB.getVehicleList();
+	    list.add("Honda new Amaze");
+	    
+	    System.out.println(vehicleA.getVehicleList());
+	    System.out.println(list);
+	    
+	    vehicleB.getVehicleList().remove("Audi A4");
+	    System.out.println(list);
+	    System.out.println(vehicleA.getVehicleList());
 	}
 
 }
