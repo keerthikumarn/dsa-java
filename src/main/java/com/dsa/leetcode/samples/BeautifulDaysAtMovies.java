a28dea2f485e4008ed7c539aae4cadee3a9d068d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-02-19 15:43:55 +0530

    small cleanup

--- src/main/java/com/dsa/leetcode/samples/BeautifulDaysAtMovies.java
+++ src/main/java/com/dsa/leetcode/samples/BeautifulDaysAtMovies.java
@@ -3,10 +3,11 @@ package com.dsa.leetcode.samples;
 public class BeautifulDaysAtMovies {
 
 	public static void main(String[] args) {
-
+		BeautifulDaysAtMovies bdm = new BeautifulDaysAtMovies();
+		System.out.println(bdm.beautifulDays(1, 100, 3));
 	}
 
-	private static int beautifulDays(int i, int j, int k) {
+	public int beautifulDays(int i, int j, int k) {
 		int daysCount = 0;
 		for (int beautifulDay = i; beautifulDay <= j; beautifulDay++) {
 			int reversedDay = reversedNum(beautifulDay);
--- src/main/java/com/dsa/leetcode/samples/HammingWeight.java
+++ src/main/java/com/dsa/leetcode/samples/HammingWeight.java
@@ -3,10 +3,11 @@ package com.dsa.leetcode.samples;
 public class HammingWeight {
 
 	public static void main(String[] args) {
-		System.out.println(hammingWeight(00000000000000000000000000010111));
+		HammingWeight weight = new HammingWeight();
+		System.out.println(weight.hammingWeight(00000000000000000000000000010111));
 	}
 
-	private static int hammingWeight(int n) {
+	public int hammingWeight(int n) {
 		int count = 0;
 		while (n > 0) {
 			count = count + (n & 1);
