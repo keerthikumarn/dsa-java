efac982bd5e14360644cf72336dbf336051c81c6
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-02-26 14:55:39 +0530

    started with the adapter design pattern

---
+++ src/main/java/com/designpatterns/adapter/BarrelCharger.java
@@ -0,0 +1,7 @@
+package com.designpatterns.adapter;
+
+public class BarrelCharger {
+	void chargeWithBarrelPort() {
+		System.out.println("Charging laptop using barrel charger.");
+	}
+}
---
+++ src/main/java/com/designpatterns/adapter/USBTypeCCharger.java
@@ -0,0 +1,5 @@
+package com.designpatterns.adapter;
+
+public interface USBTypeCCharger {
+	void chargWithUSB();
+}
