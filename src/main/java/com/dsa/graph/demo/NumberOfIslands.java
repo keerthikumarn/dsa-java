f5470e4a6cc9022e3aefc3c4dcad7b5ce7751b20
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-03-05 11:45:33 +0530

    solved a grpah problem

---
+++ src/main/java/com/dsa/graph/demo/NumberOfIslands.java
@@ -0,0 +1,39 @@
+package com.dsa.graph.demo;
+
+public class NumberOfIslands {
+
+	public static void main(String args[]) {
+
+		int graph[][] = { { 1, 1, 0, 1, 0 }, { 1, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 1 },
+				{ 1, 0, 0, 0, 0 } };
+		NumberOfIslands island = new NumberOfIslands();
+		int count = island.numberOfIsland(graph);
+		System.out.println(count);
+	}
+
+	private int numberOfIsland(int[][] graph) {
+		boolean visited[][] = new boolean[graph.length][graph[0].length];
+		int count = 0;
+		for (int idx = 0; idx < graph.length; idx++) {
+			for (int jdx = 0; jdx < graph[0].length; jdx++) {
+				if (graph[idx][jdx] == 1 && !visited[idx][jdx]) {
+					dfs(graph, idx, jdx, visited);
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	private void dfs(int[][] graph, int idx, int jdx, boolean[][] visited) {
+		if (idx < 0 || idx >= graph.length || jdx < 0 || jdx >= graph[0].length || graph[idx][jdx] == 0
+				|| visited[idx][jdx]) {
+			return;
+		}
+		visited[idx][jdx] = true;
+		dfs(graph, idx + 1, jdx, visited);
+		dfs(graph, idx - 1, jdx, visited);
+		dfs(graph, idx, jdx + 1, visited);
+		dfs(graph, idx, jdx - 1, visited);
+	}
+}
