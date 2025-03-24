792006fc1ae0c2fa4856a312a06df83ec6f79a4d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-03-24 14:09:45 +0530

    solved maximal square problem

---
+++ src/main/java/com/dsa/leetcode/samples/MaximalSquare.java
@@ -0,0 +1,27 @@
+package com.dsa.leetcode.samples;
+
+public class MaximalSquare {
+
+	public int maximalSquare(char[][] matrix) {
+		if (matrix.length == 0 || matrix[0].length == 0 || matrix[0].length == 0) {
+			return 0;
+		}
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		int[][] dp = new int[rows][cols];
+		int max = 0;
+		for (int idx = 0; idx < rows; idx++) {
+			for (int jdx = 0; jdx < cols; jdx++) {
+				if (matrix[idx][jdx] == '1') {
+					if (idx == 0 || jdx == 0) {
+						dp[idx][jdx] = 1;
+					} else {
+						dp[idx][jdx] = Math.min(dp[idx - 1][jdx], Math.min(dp[idx][jdx - 1], dp[idx - 1][jdx - 1])) + 1;
+					}
+					max = Math.max(max, dp[idx][jdx]);
+				}
+			}
+		}
+		return max;
+	}
+}
