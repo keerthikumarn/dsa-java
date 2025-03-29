7287e71a00a588e72950af8f785ef5ee85166ef1
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-03-29 22:51:54 +0530

    cleaned up

--- src/main/java/com/dsa/slidingiwindow/NumberOfNiceSubArrays.java
+++ src/main/java/com/dsa/slidingiwindow/NumberOfNiceSubArrays.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class NumberOfNiceSubArrays {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		System.out.println(numberOfSubarrays(new int[] { 1, 1, 2, 1, 1 }, 3));
 		System.out.println(numberOfSubarrays(new int[] { 2, 4, 6 }, 1));
 		System.out.println(numberOfSubarrays(new int[] { 2, 2, 2, 1, 2, 2, 1, 2, 2, 2 }, 2));
-	}
+	}*/
 
-	private static int numberOfSubarrays(int[] nums, int k) {
+	public int numberOfSubarrays(int[] nums, int k) {
 		int oddNumCount = 0;
 		int count = 0;
 		Map<Integer, Integer> prefixMap = new HashMap<>();
