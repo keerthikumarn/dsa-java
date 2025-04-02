43404a79925c6d154c39099c85a82e6924f1a007
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-04-02 07:47:08 +0530

    included classes for delivery agent and food menu

---
+++ src/main/java/com/lld/design/fooddeliverysystem/DeliveryAgent.java
@@ -0,0 +1,37 @@
+package com.lld.design.fooddeliverysystem;
+
+public class DeliveryAgent {
+
+	private final String id;
+	private final String name;
+	private final String phone;
+	private boolean available;
+
+	public DeliveryAgent(String id, String name, String phone) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.available = true;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+}
