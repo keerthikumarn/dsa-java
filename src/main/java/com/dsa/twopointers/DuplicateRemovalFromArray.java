9c81986b5596e22d84f54fa867308a8d4ded168a
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-04-17 14:33:21 +0530

    added NPE

--- src/main/java/com/dsa/twopointers/DuplicateRemovalFromArray.java
+++ src/main/java/com/dsa/twopointers/DuplicateRemovalFromArray.java
@@ -2,15 +2,16 @@ package com.dsa.twopointers;
 
 public class DuplicateRemovalFromArray {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
         int newLength = removeDuplicates(nums);
         for (int idx = 0; idx < newLength; idx++) {
             System.out.print(nums[idx] + " ");
-        }	}
+        }	
+	}*/
 
-	public static int removeDuplicates(int[] nums) {
-		if (nums.length == 0) {
+	public int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0) {
 			return -1;
 		}
 		int slowPtr = 0;
