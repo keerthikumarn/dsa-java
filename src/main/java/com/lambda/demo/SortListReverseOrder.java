cd61c4e4e9e497d13e5f4a87feacc70625ed978d
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-07-05 22:09:54 +0530

    implemented junits

--- src/main/java/com/lambda/demo/SortListReverseOrder.java
+++ src/main/java/com/lambda/demo/SortListReverseOrder.java
@@ -2,10 +2,12 @@ package com.lambda.demo;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortListReverseOrder {
 
-	public void sortListInReverseOrder(List<String> list) {
-		list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+	public List<String> sortListInReverseOrder(List<String> list) {
+		return list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
 	}
+	
 }
