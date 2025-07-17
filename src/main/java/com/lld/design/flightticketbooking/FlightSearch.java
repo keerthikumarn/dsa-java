ba54fbd7816740a850f506ed9e68494975ba9cb5
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-17 22:38:23 +0530

    implemented flight search

---
+++ src/main/java/com/lld/design/flightticketbooking/FlightSearch.java
@@ -0,0 +1,28 @@
+package com.lld.design.flightticketbooking;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightSearch {
+
+	private List<Flight> flights;
+
+	public FlightSearch() {
+		flights = new ArrayList<>();
+	}
+
+	public void addFlight(Flight flight) {
+		flights.add(flight);
+	}
+
+	public List<Flight> searchFlights(String source, String destination, LocalDate date) {
+		return flights.stream()
+				.filter(flight -> flight.getSource().equalsIgnoreCase(source)
+						&& flight.getDestination().equalsIgnoreCase(destination)
+						&& flight.getDepartureTime().equals(date))
+				.collect(Collectors.toList());
+	}
+
+}
