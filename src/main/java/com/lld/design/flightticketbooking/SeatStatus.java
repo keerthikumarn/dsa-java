a7307dc160e2c5860b601b5256e8da6bc25e878c
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-17 22:38:59 +0530

    created enums for seats

---
+++ src/main/java/com/lld/design/flightticketbooking/SeatStatus.java
@@ -0,0 +1,5 @@
+package com.lld.design.flightticketbooking;
+
+public enum SeatStatus {
+	AVAILABLE, BOOKED, RESERVED, UNAVAILABLE
+}
---
+++ src/main/java/com/lld/design/flightticketbooking/SeatType.java
@@ -0,0 +1,5 @@
+package com.lld.design.flightticketbooking;
+
+public enum SeatType {
+	ECONOMY, BUSINESS, FIRST_CLASS, PREMIUM_ECONOMY, EXECUTIVE_CLASS
+}
