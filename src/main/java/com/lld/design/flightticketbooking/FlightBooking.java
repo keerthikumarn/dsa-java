58997479faad82ec2be9ced170875ef65732ff2d
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-18 18:54:47 +0530

    included flight booking manager class

---
+++ src/main/java/com/lld/design/flightticketbooking/FlightBooking.java
@@ -0,0 +1,31 @@
+package com.lld.design.flightticketbooking;
+
+import java.util.UUID;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class FlightBooking {
+	private String id;
+    private Flight flight;
+    private Passenger passenger;
+    private Seat seat;
+    private double price;
+    private FlightBookingStatus status;
+    
+    public FlightBooking(Flight flight, Passenger passenger, Seat seat, double price) {
+        this.id = UUID.randomUUID().toString();
+        this.flight = flight;
+        this.passenger = passenger;
+        this.seat = seat;
+        this.price = price;
+        this.status = FlightBookingStatus.CONFIRMED;
+    }
+    
+    public void cancelBooking() {
+    	this.status = FlightBookingStatus.CANCELLED;
+    	seat.release();
+    }
+}
---
+++ src/main/java/com/lld/design/flightticketbooking/FlightBookingManager.java
@@ -0,0 +1,49 @@
+package com.lld.design.flightticketbooking;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.lld.design.concertbooking.Booking;
+
+public class FlightBookingManager {
+
+	private static FlightBookingManager instance;
+	private Map<String, FlightBooking> bookings;
+	private Object lock = new Object();
+
+	private FlightBookingManager() {
+		// Initialize bookings map
+		this.bookings = new HashMap<>();
+	}
+
+	public static FlightBookingManager getInstance() {
+		if (instance == null) {
+			synchronized (FlightBookingManager.class) {
+				if (instance == null) {
+					instance = new FlightBookingManager();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public FlightBooking createBooking(Flight flight, Passenger passenger, Seat seat, double price) {
+		String bookingNumber = UUID.randomUUID().toString();
+		FlightBooking booking = new FlightBooking(flight, passenger, seat, price);
+		synchronized (lock) {
+			bookings.put(bookingNumber, booking);
+		}
+		return booking;
+	}
+
+	public void cancelBooking(String bookingNumber) {
+		synchronized (lock) {
+			FlightBooking booking = bookings.get(bookingNumber);
+			if (booking != null) {
+				booking.cancelBooking();
+			}
+		}
+	}
+
+}
