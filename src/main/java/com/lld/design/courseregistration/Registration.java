f90b8beb985692095986fa60310d6bbfc2256300
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-21 16:34:13 +0530

    started with LLD of course reg

---
+++ src/main/java/com/lld/design/courseregistration/Registration.java
@@ -0,0 +1,15 @@
+package com.lld.design.courseregistration;
+
+import java.sql.Timestamp;
+
+public class Registration {
+	private Student student;
+    private Course course;
+    private Timestamp registrationTime;
+
+    public Registration(Student student, Course course, Timestamp registrationTime) {
+        this.student = student;
+        this.course = course;
+        this.registrationTime = registrationTime;
+    }
+}
