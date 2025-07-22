1e6b7a13a95ffccb6fcacef4c7da52d77c78c3ae
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-22 17:14:36 +0530

    updated seat class

--- src/main/java/com/lld/design/flightticketbooking/Seat.java
+++ src/main/java/com/lld/design/flightticketbooking/Seat.java
@@ -7,10 +7,34 @@ public class Seat {
 	private String seatNumber;
 	private SeatType type;
 	private SeatStatus status;
-	
+
 	public Seat(String seatNumber, SeatType type) {
 		this.seatNumber = seatNumber;
 		this.type = type;
 		this.status = SeatStatus.AVAILABLE;
+	}
+
+	public void reserve() {
+		if (this.status == SeatStatus.AVAILABLE) {
+			this.status = SeatStatus.RESERVED;
+		} else {
+			throw new IllegalStateException("Seat is not available for reservation.");
+		}
+	}
+
+	public void release() {
+		if (this.status == SeatStatus.RESERVED) {
+			this.status = SeatStatus.AVAILABLE;
+		} else {
+			throw new IllegalStateException("Seat is not reserved, cannot release.");
+		}
+	}
+
+	public synchronized void book() {
+		if (this.status == SeatStatus.AVAILABLE) {
+			this.status = SeatStatus.BOOKED;
+		} else {
+			throw new IllegalStateException("Seat is not available for booking.");
+		}
 	}
 }
