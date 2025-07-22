dd3434e1cc1fb2fc68eb7624ade7fb7154be32dc
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-22 23:19:37 +0530

    started with AirlineManagementSystem

---
+++ src/main/java/com/lld/design/flightticketbooking/AirlineManagementSystem.java
@@ -0,0 +1,30 @@
+package com.lld.design.flightticketbooking;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AirlineManagementSystem {
+
+	private Map<String, Flight> flights;
+	private Map<String, Aircraft> aircrafts;
+	private Map<String, Passenger> passengers;
+	private FlightSearch flightSearch;
+	private FlightBookingManager bookingManager;
+	private PaymentProcessor paymentProcessor;
+
+	public AirlineManagementSystem() {
+		flights = new HashMap<>();
+		aircrafts = new HashMap<>();
+		passengers = new HashMap<>();
+		flightSearch = new FlightSearch();
+		bookingManager = FlightBookingManager.getInstance();
+		paymentProcessor = PaymentProcessor.getInstance();
+	}
+	
+	public Passenger addPassenger(String name, String email) {
+        Passenger passenger = new Passenger(name, email);
+        passengers.put(passenger.getId(), passenger);
+        return passenger;
+    }
+
+}
