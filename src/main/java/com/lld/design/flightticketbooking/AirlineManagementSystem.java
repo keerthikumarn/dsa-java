b41ad877cf0042667036c21bdc811d718e6f6494
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-07-28 16:38:34 +0530

    added the client class

--- src/main/java/com/lld/design/flightticketbooking/AirlineManagementSystem.java
+++ src/main/java/com/lld/design/flightticketbooking/AirlineManagementSystem.java
@@ -1,6 +1,9 @@
 package com.lld.design.flightticketbooking;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AirlineManagementSystem {
@@ -20,17 +23,44 @@ public class AirlineManagementSystem {
 		bookingManager = FlightBookingManager.getInstance();
 		paymentProcessor = PaymentProcessor.getInstance();
 	}
-	
+
 	public Passenger addPassenger(String name, String email) {
-        Passenger passenger = new Passenger(name, email);
-        passengers.put(passenger.getId(), passenger);
-        return passenger;
-    }
-	
+		Passenger passenger = new Passenger(name, email);
+		passengers.put(passenger.getId(), passenger);
+		return passenger;
+	}
+
+	public Flight addFlight(String source, String destination, LocalDateTime departure, LocalDateTime arrival,
+			String aircraftNumber) {
+		Aircraft aircraft = aircrafts.get(aircraftNumber);
+		Flight flight = new Flight(source, destination, aircraftNumber, departure, arrival, null, aircraft, null, null);
+		flights.put(flight.getFlightNumber(), flight);
+		flightSearch.addFlight(flight);
+		return flight;
+	}
+
 	public Aircraft addAircraft(String tailNumber, String model, int totalSeats) {
-        Aircraft aircraft = new Aircraft(tailNumber, model, totalSeats);
-        aircrafts.put(tailNumber, aircraft);
-        return aircraft;
-    }
+		Aircraft aircraft = new Aircraft(tailNumber, model, totalSeats);
+		aircrafts.put(tailNumber, aircraft);
+		return aircraft;
+	}
+
+	public List<Flight> searchFlights(String source, String destination, LocalDate date) {
+		return flightSearch.searchFlights(source, destination, date);
+	}
+
+	public FlightBooking bookFlight(String flightNumber, String passengerId, Seat seat, double price) {
+		Flight flight = flights.get(flightNumber);
+		Passenger passenger = passengers.get(passengerId);
+		return bookingManager.createBooking(flight, passenger, seat, price);
+	}
+
+	public void cancelBooking(String bookingNumber) {
+		bookingManager.cancelBooking(bookingNumber);
+	}
+
+	public void processPayment(Payment payment) {
+		paymentProcessor.processPayment(payment);
+	}
 
 }
