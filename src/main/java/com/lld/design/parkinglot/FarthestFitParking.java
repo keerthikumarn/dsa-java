654ad478b7a03580d6d7537745d2f519c9e0b7b0
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-08-15 11:38:40 +0530

    parking lot logic

---
+++ src/main/java/com/lld/design/parkinglot/FarthestFitParking.java
@@ -0,0 +1,24 @@
+package com.lld.design.parkinglot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class FarthestFitParking implements Parking {
+
+	@Override
+	public Optional<ParkingSpot> findSpot(List<ParkingFloor> floors, Vehicle vehicle) {
+		 List<ParkingFloor> reversedFloors = new ArrayList<>(floors);
+	        Collections.reverse(reversedFloors);
+	        for (ParkingFloor floor : reversedFloors) {
+	            Optional<ParkingSpot> spot = floor.findAvailableSpot(vehicle);
+	            if (spot.isPresent()) {
+	                return spot;
+	            }
+	        }
+	        return Optional.empty();
+	    }
+	}
+
+}
---
+++ src/main/java/com/lld/design/parkinglot/ParkingLot.java
@@ -0,0 +1,5 @@
+package com.lld.design.parkinglot;
+
+public class ParkingLot {
+
+}
