882bccae517f1a338992ef238f792cb0b53dc5db
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-08-16 13:42:12 +0530

    logic impl of parking lot

--- src/main/java/com/lld/design/parkinglot/ParkingLot.java
+++ src/main/java/com/lld/design/parkinglot/ParkingLot.java
@@ -1,5 +1,36 @@
 package com.lld.design.parkinglot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ParkingLot {
 
+	private static ParkingLot instance;
+	private List<ParkingFloor> floors = new ArrayList<>();
+	private Map<String, ParkingTicket> activeTickets;
+	private ParkingFee feeStrategy;
+	private Parking parkingStrategy;
+
+	private ParkingLot() {
+		this.feeStrategy = new FlatRateFee();
+		this.parkingStrategy = new BestFitParking();
+		this.activeTickets = new ConcurrentHashMap<>();
+	}
+
+	public static synchronized ParkingLot getInstance() {
+		if (instance == null) {
+			instance = new ParkingLot();
+		}
+		return instance;
+	}
+
+	public void addFloor(ParkingFloor floor) {
+		floors.add(floor);
+	}
+	
+	public void setParkingStrategy(Parking parkingStrategy) {
+		this.parkingStrategy = parkingStrategy;
+	}
 }
