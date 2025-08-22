4e3db70f8286c3d43d8b68bf17cbc6171c6c985f
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-08-22 10:16:07 +0530

    fixed a small issue

--- src/main/java/com/lld/design/parkinglot/FarthestFitParking.java
+++ src/main/java/com/lld/design/parkinglot/FarthestFitParking.java
@@ -9,16 +9,14 @@ public class FarthestFitParking implements Parking {
 
 	@Override
 	public Optional<ParkingSpot> findSpot(List<ParkingFloor> floors, Vehicle vehicle) {
-		 List<ParkingFloor> reversedFloors = new ArrayList<>(floors);
-	        Collections.reverse(reversedFloors);
-	        for (ParkingFloor floor : reversedFloors) {
-	            Optional<ParkingSpot> spot = floor.findAvailableSpot(vehicle);
-	            if (spot.isPresent()) {
-	                return spot;
-	            }
-	        }
-	        return Optional.empty();
-	    }
+		List<ParkingFloor> reversedFloors = new ArrayList<>(floors);
+		Collections.reverse(reversedFloors);
+		for (ParkingFloor floor : reversedFloors) {
+			Optional<ParkingSpot> spot = floor.findAvailableSpot(vehicle);
+			if (spot.isPresent()) {
+				return spot;
+			}
+		}
+		return Optional.empty();
 	}
-
 }
