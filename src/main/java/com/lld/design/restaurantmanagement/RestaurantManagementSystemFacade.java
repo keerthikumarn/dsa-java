c214940b617f77128cf6e4e50343ca1b1db675df
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-09-03 23:21:40 +0530

    implemented functionalities of adding waiter, table and staff

---
+++ src/main/java/com/lld/design/restaurantmanagement/RestaurantManagementSystemFacade.java
@@ -0,0 +1,50 @@
+package com.lld.design.restaurantmanagement;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RestaurantManagementSystemFacade {
+
+	private static RestaurantManagementSystemFacade instance = null;
+	private static Restaurant restaurant = Restaurant.getInstance();
+	private AtomicInteger orderIdCounter;
+	private Map<Integer, Order> orders;
+
+	private RestaurantManagementSystemFacade() {
+		this.orderIdCounter = new AtomicInteger(1);
+		this.orders = new HashMap<>();
+	}
+
+	public static RestaurantManagementSystemFacade getInstance() {
+		if (instance == null) {
+			instance = new RestaurantManagementSystemFacade();
+		}
+		return instance;
+	}
+
+	public Table addTable(int tableId, int capacity) {
+		Table table = new Table(tableId, capacity);
+		restaurant.addTable(table);
+		return table;
+	}
+
+	public Waiter addWaiter(int id, String name) {
+		Waiter waiter = new Waiter(id, name);
+		restaurant.addWaiter(waiter);
+		return waiter;
+	}
+
+	public Chef addChef(int id, String name) {
+		Chef chef = new Chef(id, name);
+		restaurant.addChef(chef);
+		return chef;
+	}
+
+	public MenuItem addMenuItem(String id, String name, double price) {
+		MenuItem item = new MenuItem(id, name, price);
+		restaurant.getMenu().addItem(item);
+		return item;
+	}
+
+}
