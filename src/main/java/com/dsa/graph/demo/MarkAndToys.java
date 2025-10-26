cda7dacb963c135f98cdec4f556f0c27fce08021
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-10-26 23:01:51 +0530

    solved marks and toys problem

---
+++ src/main/java/com/dsa/graph/demo/MarkAndToys.java
@@ -0,0 +1,22 @@
+package com.dsa.graph.demo;
+
+import java.util.Arrays;
+
+public class MarkAndToys {
+
+	public int maximumToys(int[] prices, int amountToSpend) {
+		int priceLen = prices.length;
+		Arrays.sort(prices);
+		int toyCount = 0;
+		int totalSpent = 0;
+		for (int idx = 0; idx < priceLen; idx++) {
+			if (totalSpent + prices[idx] <= amountToSpend) {
+				totalSpent += prices[idx];
+				toyCount++;
+			} else {
+				break;
+			}
+		return toyCount;
+	}
+
+}
